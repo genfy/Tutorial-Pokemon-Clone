6721702e5d3ca38d20be1a9956176f26ec66d15b
genfy <alexmainstone@atheist.com>
2016-08-30 15:24:10 +0100

    Tutorial #6
    Finished Dialogue system!

--- Project/scripts/Dialogue Box.gd
+++ Project/scripts/Dialogue Box.gd
@@ -2,32 +2,56 @@
 extends Patch9Frame
 
 var printing = false
+var donePrinting = false
+
+var pressed = false
+
 var timer = 0
-var textToPrint = ""
+var textToPrint = []
 
 var currentChar = 0
+var currentText = 0
 
 const SPEED = 0.1
 
 func _ready():
 	set_fixed_process(true)
+	set_process_unhandled_key_input(true)
+
+func _unhandled_key_input(key_event):
+	if key_event.is_action_pressed("ui_interact"):
+		pressed = true
+	elif key_event.is_action_released("ui_interact"):
+		pressed = false
 
 func _fixed_process(delta):
 	if printing:
-		timer += delta
-		if timer > SPEED:
-			timer = 0
-			get_node("RichTextLabel").set_bbcode(get_node("RichTextLabel").get_bbcode() + textToPrint[currentChar])
-			currentChar += 1
-		
-		if currentChar >= textToPrint.length():
-			currentChar = 0
-			textToPrint = ""
-			printing = false
-			timer = 0
+		if !donePrinting:
+			timer += delta
+			if timer > SPEED:
+				timer = 0
+				get_node("RichTextLabel").set_bbcode(get_node("RichTextLabel").get_bbcode() + textToPrint[currentText][currentChar])
+				currentChar += 1
+			
+			if currentChar >= textToPrint[currentText].length():
+				currentChar = 0
+				timer = 0
+				donePrinting = true
+				currentText += 1
+		elif pressed:
+			donePrinting = false
+			get_node("RichTextLabel").set_bbcode("")
+			if currentText >= textToPrint.size():
+				currentText = 0
+				textToPrint = []
+				printing = false
+				set_hidden(true)
+				get_node("/root/world/Player").canMove = true
+	
+	pressed = false
 
 
 func _print_dialogue(text):
 	textToPrint = text
 	printing = true
-	
+	get_node("/root/world/Player").canMove = false
--- Project/scripts/player.gd
+++ Project/scripts/player.gd
@@ -5,6 +5,9 @@ var direction = Vector2(0,0)
 var startPos = Vector2(0,0)
 var moving = false
 
+var canMove = true
+var pressed = false
+
 const SPEED = 1
 const GRID = 16
 
@@ -16,11 +19,18 @@ var animationPlayer
 func _ready():
 	world = get_world_2d().get_direct_space_state()
 	set_fixed_process(true)
+	set_process_input(true)
 	sprite = get_node("Sprite")
 	animationPlayer = get_node("AnimationPlayer")
 
+func _input(event):
+	if event.is_action_pressed("ui_interact"):
+		pressed = true
+	elif event.is_action_released("ui_interact"):
+		pressed = false
+
 func _fixed_process(delta):
-	if !moving:
+	if !moving and canMove:
 		var resultUp = world.intersect_point(get_pos() + Vector2(0, -GRID))
 		var resultDown = world.intersect_point(get_pos() + Vector2(0, GRID))
 		var resultLeft = world.intersect_point(get_pos() + Vector2(-GRID, 0))
@@ -55,7 +65,7 @@ func _fixed_process(delta):
 				startPos = get_pos()
 				animationPlayer.play("walk_right")
 		
-		if Input.is_action_pressed("ui_interact"):
+		if pressed:
 			if sprite.get_frame() == 0:
 				interact(resultUp)
 			elif sprite.get_frame() == 6:
@@ -64,13 +74,14 @@ func _fixed_process(delta):
 				interact(resultLeft)
 			elif sprite.get_frame() == 9:
 				interact(resultRight)
-	else:
+	elif canMove:
 		move_to(get_pos() + direction * SPEED)
 		if get_pos() == startPos + Vector2(GRID * direction.x, GRID * direction.y):
 			moving = false
+	pressed = false
 
 func interact(result):
 	for dictionary in result:
 		if typeof(dictionary.collider) == TYPE_OBJECT and dictionary.collider.has_node("Interact"):
 			get_node("Camera2D/Dialogue Box").set_hidden(false)
-			get_node("Camera2D/Dialogue Box")._print_dialogue("Hello World!\nHello Again World!")
+			get_node("Camera2D/Dialogue Box")._print_dialogue(dictionary.collider.get_node("Interact").text)
